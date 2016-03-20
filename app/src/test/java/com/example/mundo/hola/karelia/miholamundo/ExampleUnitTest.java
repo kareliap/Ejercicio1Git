6c6916cee644dce1000e85e2a2eee98932901dbe
WIN8.1 <karelia_p@hotmail.com>
2016-03-20 11:05:52 -0430

    Initial Commit

---
+++ app/src/test/java/com/example/mundo/hola/karelia/miholamundo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.mundo.hola.karelia.miholamundo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
